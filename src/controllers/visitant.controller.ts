4fa7f9779d9a0376cf9f3b708ae7d49af4a89dd0
Dudu2231 <eduardonascimento2231@gmail.com>
2025-12-05 18:05:32 -0300

    add: await ao bcrypt

--- src/controllers/visitant.controller.ts
+++ src/controllers/visitant.controller.ts
@@ -1,12 +1,10 @@
 import type { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { user} from "../utils.js";
+import { user } from "../utils.js";
 import bcrypt from "bcrypt";
-import e from "express";
 
 const visitant = {
   getHomePage: async (req: Request, res: Response) => {
-    
     res.json({ pagina: "Voce é um visitante e está na página Home" });
   },
 
@@ -17,15 +15,24 @@ const visitant = {
     const { email, password } = req.body;
     if (!email || !password)
       return res.status(400).json({ erro: "Insira um email e uma senha" });
-    const hasUser = await user.findFirst({where: {email: email}})
-    
-    if (!hasUser) return res.status(400).json({erro: 'Credenciais não encontradas'})
-    
-    const passwordMatches = bcrypt.compare(password, hasUser.password)
-    if (!passwordMatches) return res.status(400).json({erro: "Credenciais inválidas"})
-    const token = jwt.sign({email: hasUser.email, id: hasUser.id}, process.env.SECRET_PHRASE, {
-      expiresIn: "1h",
-    });
+    const hasUser = await user.findFirst({ where: { email: email } });
+
+    if (!hasUser)
+      return res
+        .status(400)
+        .json({
+          erro: "Credenciais não encontradas, certifique-se que o email atual está cadastrado",
+        });
+    const passwordMatches = await bcrypt.compare(password, hasUser.password);
+    if (!passwordMatches)
+      return res.status(400).json({ erro: "Credenciais inválidas" });
+    const token = jwt.sign(
+      { email: hasUser.email, id: hasUser.id },
+      process.env.SECRET_PHRASE,
+      {
+        expiresIn: "1h",
+      },
+    );
     res.cookie("access-token", token, {
       httpOnly: true,
       maxAge: 60 * 60 * 100,
@@ -36,14 +43,15 @@ const visitant = {
   register: async (req: Request, res: Response) => {
     if (!process.env.SECRET_PHRASE) {
       throw new Error("Parece que o segredo do JWT não foi definido no .env");
-    } 
+    }
     const { email, password, name } = req.body;
     try {
-      const hasUser = await user.findFirst({where: {email: email}})
+      const hasUser = await user.findFirst({ where: { email: email } });
 
-      if (hasUser) return res.status(400).json({erro: "Esse email já está em uso"})
+      if (hasUser)
+        return res.status(400).json({ erro: "Esse email já está em uso" });
       const hashedPasssword = await bcrypt.hash(password, 10);
-      
+
       const createdUser = await user.create({
         data: { email: email, name: name, password: hashedPasssword },
       });
@@ -57,12 +65,10 @@ const visitant = {
       );
 
       res.cookie("access-token", token, { maxAge: 3600000, httpOnly: true });
-      return res
-        .status(201)
-        .json({
-          mensagem: "Parabens, você está cadastrado",
-          user: { id: createdUser.id, name: createdUser.email },
-        });
+      return res.status(201).json({
+        mensagem: "Parabens, você está cadastrado",
+        user: { id: createdUser.id, name: createdUser.email },
+      });
     } catch (e) {
       console.error(e);
       return res
